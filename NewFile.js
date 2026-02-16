d69096b852718c4b7ce762aa78a54c17c009cd37
Abhishek Sharma <as6911904@gmail.com>
2026-02-16 12:00:36 +0530

    Next Day

---
+++ NewFile.js
@@ -0,0 +1,21 @@
+// [
+//     "node",
+//     "newFile.js",
+//     "add",
+//     "1",
+//     "2"
+// ]
+
+
+const pr = process.argv[2]
+const value1=process.argv[3]
+const value2=process.argv[4]
+ 
+
+
+if(pr==="add"){
+    console.log("~process",(Number)(value1)+(Number)(value2));
+}
+if(pr==="multiply"){
+    console.log("~process",value1*value2);
+}
--- file1.js
+++ file1.js
@@ -22,15 +22,15 @@
 
 
 
-const login=()=>{
-    return Promise.resolve("Login Succsessful")
-};
-const getUser=()=>{
-    return Promise.resolve("User Data");
-}
-const getUserName=()=>{
-    return Promise.resolve("User Name");
-}
+// const login=()=>{
+//     return Promise.resolve("Login Succsessful")
+// };
+// const getUser=()=>{
+//     return Promise.resolve("User Data");
+// }
+// const getUserName=()=>{
+//     return Promise.resolve("User Name");
+// }
 // login().then((err,response) => {
 //     if(response){
 //         console.log("Login failed");
@@ -45,30 +45,52 @@ const getUserName=()=>{
 // });
 
 
-login()
-    .then(getUser)
-    .then(getUserName)
-    .then(console.log("All operations completed"))
-    .catch((err)=> console.error("Error",err));
+// login()
+//     .then(getUser)
+//     .then(getUserName)
+//     .then(console.log("All operations completed"))
+//     .catch((err)=> console.error("Error",err));
 
 
-Promise.resolve(10)
-    .then((x)=>x+5)
-    .then((x)=>x+20)
-    .then(console.log())
+// Promise.resolve(10)
+//     .then((x)=>x+5)
+//     .then((x)=>x+20)
+//     .then(console.log())
+
+
+// const sample=async()=>{
+//     console.log("Sample content");
+     
+//     await fetch("https://jsonplaceholder.typicode.com/posts/1")
+//     .then((response)=> response.json())
+//     .then((json)=> console.log(json));
+//     console.log('~ sample "Ended":',"Ended");
+// };
+
+// console.log('~sample ~ "Started":',"STarted--------1");
+// sample();
+// console.log('~sample ~"Started":',"Started--------2");
 
 
 const sample=async()=>{
-    console.log("Sample content");
-     
-    await fetch("https://jsonplaceholder.typicode.com/posts/1")
-    .then((response)=> response.join())
-    .then((json)=> console.log(json));
-    console.log('~ sample "Ended":',"Ended");
-};
+    const func1 =fetch("https://jsonplaceholder.typicode.com/posts/1")
+        .then((response) => response.json())
+        // .then((json)=> console.log(json));
 
-console.log('~sample ~ "Started":',"STarted--------1");
+    const func2 =fetch("https://jsonplaceholder.typicode.com/posts/1")
+        .then((response) => response.json())
+        // .then((json)=> console.log(json));
+    const a =await func1;
+    const b=await func2;
+    console.log("~sample ~b",b);
+    console.log("~sample ~a",a);
+};
 sample();
-console.log('~sample ~"Started":',"Started--------2");
     
 
+
+//URL 
+// https:// -> Protocols
+// www.amazon.in -> Domain
+// /s -> Path parameter
+// ?k=motorola+phone+5g&rh=p_36%3A2000000-%2Cp_123%3A146762%257C253649%2Cp_90%3A20912642031%2Cp_n_g-1003492455111%3A81332996031%2Cp_n_g-1002085472111%3A27355812031%2Cp_n_g-1003469290111%3A33962382031&dc&crid=HBEILRPC8FXF&qid=1771221844&rnid=33962288031&sprefix=mo%2Caps%2C367&ref=sr_nr_p_n_g-1003469290111_1&ds=v1%3A5mgXoSP81eltjwivPyOh2yRtDbPDEH4zXOVlQpRLYqw  ->Query Parameter
